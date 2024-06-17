633932f8f1906e4edc6ffd19ec02b60a53264a28
«irikuz» <i-iri.kuz@yandex.ru>
2024-06-17 12:25:58 +0300

    Initial commit

---
+++ homework5.py
@@ -0,0 +1,12 @@
+tuple = 1, 4, True, "apple"
+immutable_var = tuple
+print(immutable_var)
+
+
+
+
+#immutable_var[0]=[200] кортеж не поддерживает обращение по элементам
+
+mutable_list = [1, 3, "apple", True]
+mutable_list [0]="banana"
+print(mutable_list)
